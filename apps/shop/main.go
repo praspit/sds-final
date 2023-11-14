fda653d3e16a86bf85b57bd196b9058df02ecc99
praspit <praspit@outlook.com>
2023-11-14 14:19:53 +0700

    init project

---
+++ apps/shop/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	shopMap := map[string]map[string]string{
+		"shop1": {
+			"shopName": "shop1",
+			"shopAddr": "shop1Addr",
+		},
+		"shop2": {
+			"shopName": "shop2",
+			"shopAddr": "shop2Addr",
+		},
+		"shop3": {
+			"shopName": "shop3",
+			"shopAddr": "shop3Addr",
+		},
+	}
+
+	r.GET("/", func(c *gin.Context) { c.JSON(200, "Hello, this is shop service") })
+
+	r.GET("/shop", func(c *gin.Context) { c.JSON(200, shopMap) })
+
+	r.GET("/shop/:shopName", func(c *gin.Context) {
+		shopName := c.Param("shopName")
+		c.JSON(200, shopMap[shopName])
+	})
+
+	r.Run(":8000")
+}
