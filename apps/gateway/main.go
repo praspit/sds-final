3dce1e99a0e66aba0e206b0f0639adc152e1ac95
praspit <praspit@outlook.com>
2023-11-16 22:39:39 +0700

    finalize gateway/shop service for apps

--- apps/gateway/main.go
+++ apps/gateway/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
 	"sds-final/common"
 
 	"github.com/gin-gonic/gin"
 )
 
+var client *http.Client
+
 func main() {
 	r := gin.Default()
-	client := &http.Client{}
+	client = &http.Client{}
 
 	r.GET("/", func(c *gin.Context) { c.JSON(200, "Hello, this is gateway service") })
 	r.GET("/health-check", func(c *gin.Context) { c.JSON(200, "ok") })
@@ -22,19 +22,40 @@ func main() {
 		shop := getAllShops(client)
 		c.JSON(200, shop)
 	})
+
+	r.GET("/payment", func(c *gin.Context) {
+		payment := getPayment(client)
+		c.JSON(200, payment)
+	})
+
+	r.Run(":8000")
 }
 
-func getAllShops(client *http.Client) map[string]map[string]string {
-	path := fmt.Sprintf("http://%s:%s/shop", os.Getenv("SHOP_HOST"), os.Getenv("SHOP_PORT"))
-	res, err := client.Get(path)
+func getAllShops(client *http.Client) map[string]string {
+	path := "http://shop-service-service:8000/shop"
+	var shopRes common.MapResponse
+	err := GetJson(path, &shopRes)
 	if err != nil {
-		return map[string]map[string]string{}
-	}
-	defer res.Body.Close()
-	var shopRes common.MapOfMapResponse
-	err = json.NewDecoder(res.Body).Decode(&shopRes)
-	if err != nil {
-		return map[string]map[string]string{}
+		return map[string]string{}
 	}
 	return shopRes.Data
+}
+
+func getPayment(client *http.Client) map[string]int {
+	path := "http://payment-service-service:8081/payment"
+	var paymentRes map[string]int
+	err := GetJson(path, &paymentRes)
+	if err != nil {
+		return map[string]int{}
+	}
+	return paymentRes
+}
+
+func GetJson(url string, target interface{}) error {
+	res, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	return json.NewDecoder(res.Body).Decode(target)
 }
--- apps/shop/main.go
+++ apps/shop/main.go
@@ -1,39 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"sds-final/common"
 
 	"github.com/gin-gonic/gin"
 )
 
+var client *http.Client
+
+type Item struct {
+	Price []int    `json:"price"`
+	Count []int    `json:"count"`
+	Item  []string `json:"item"`
+}
+
 func main() {
 	r := gin.Default()
+	client = &http.Client{}
 
-	shopMap := map[string]map[string]string{
-		"shop1": {
-			"shopName": "shop1",
-			"shopAddr": "shop1Addr",
-		},
-		"shop2": {
-			"shopName": "shop2",
-			"shopAddr": "shop2Addr",
-		},
-		"shop3": {
-			"shopName": "shop3",
-			"shopAddr": "shop3Addr",
-		},
+	shopMap := map[string]string{
+		"shopName": "shop1",
+		"shopAddr": "shop1Addr",
 	}
 
 	r.GET("/", func(c *gin.Context) { c.JSON(200, "Hello, this is shop service") })
 
-	r.GET("/shop", func(c *gin.Context) { c.JSON(200, common.MapOfMapResponse{Data: shopMap}) })
-
-	r.GET("/shop/:shopName", func(c *gin.Context) {
-		shopName := c.Param("shopName")
-		c.JSON(200, common.MapResponse{Data: shopMap[shopName]})
+	r.GET("/shop", func(c *gin.Context) {
+		path := "http://inventory-service-service:8003/items"
+		var items Item
+		err := GetJson(path, &items)
+		if err != nil {
+			c.JSON(500, "error")
+		}
+		shopMap["items"] = fmt.Sprintf("%v", items)
+		c.JSON(200, common.MapResponse{Data: shopMap})
 	})
 
 	r.GET("/health-check", func(c *gin.Context) { c.JSON(200, "ok") })
 
 	r.Run(":8000")
+}
+
+func GetJson(url string, target interface{}) error {
+	res, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	return json.NewDecoder(res.Body).Decode(target)
 }
